9cd02f43a495c9fcc49b11e2d76822255d27bbc2
KyungMin Lee <hello.tidavid@gmail.com>
2024-06-26 00:43:58 +0900

    define Security Filter Chain

--- src/main/java/org/example/coin_laundry_app_backend/config/security/SecurityConfig.java
+++ src/main/java/org/example/coin_laundry_app_backend/config/security/SecurityConfig.java
@@ -1,4 +1,46 @@
 package org.example.coin_laundry_app_backend.config.security;
 
+import lombok.RequiredArgsConstructor;
+import org.example.coin_laundry_app_backend.config.security.jwt.JWTAuthenticationFilter;
+import org.example.coin_laundry_app_backend.config.security.jwt.JWTHelper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JWTHelper jwtHelper;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .rememberMe(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .sessionManagement(
+                management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(
+                requests -> {
+                    requests.requestMatchers("/api/v1/sign/**").permitAll();
+                    requests.anyRequest().authenticated();
+                }
+            )
+            .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
+
+    @Bean
+    JWTAuthenticationFilter jwtAuthenticationFilter() {
+        return new JWTAuthenticationFilter(jwtHelper);
+    }
 }
