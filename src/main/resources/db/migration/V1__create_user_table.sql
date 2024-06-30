789fbda824f91add82590f05b09c05a5c8e84cfb
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-01 00:32:16 +0900

    flyway sql update - modify `users` table - add `verification_codes` table

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,6 +1,18 @@
 -- V1__create_user_table.sql
-CREATE TABLE users (
-                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       name VARCHAR(100) NOT NULL,
-                       email VARCHAR(100) NOT NULL
+CREATE TABLE users
+(
+    id            BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username      VARCHAR(100) NOT NULL,
+    phone_number  VARCHAR(14)  NOT NULL,
+    commercial_yn TINYINT      NOT NULL,
+    location_yn   TINYINT      NOT NULL,
+);
+
+CREATE TABLE verification_codes
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    phone_number VARCHAR(14)  NOT NULL,
+    code         VARCHAR(100) NOT NULL,
+    created_at   TIMESTAMP    NOT NULL,
+    expired_at   TIMESTAMP    NOT NULL,
 );
