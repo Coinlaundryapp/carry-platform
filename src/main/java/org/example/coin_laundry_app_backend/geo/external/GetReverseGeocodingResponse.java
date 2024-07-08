62bf53bd900ee22f89547fc36c1c47d76d65dbaf
siwony <official.siwony@gmail.com>
2024-07-08 14:14:00 +0900

    코틀린 관련 코드 제거

--- src/main/java/org/example/coin_laundry_app_backend/geo/external/GetReverseGeocodingResponse.java
+++ src/main/java/org/example/coin_laundry_app_backend/geo/external/GetReverseGeocodingResponse.java
@@ -12,7 +12,9 @@ public record GetReverseGeocodingResponse(Status status, List<Result> results) {
 
     public record Region(Area area0, Area area1, Area area2, Area area3, Area area4) {}
 
-    public record Area(String name, Coords coords, String alias) {}
+    public record Area(String name, Coords coords, String alias) {
+        final static Area EMPTY = new Area("", new Coords(new Center("", 0, 0)), "");
+    }
 
     public record Coords(Center center) {}
 
