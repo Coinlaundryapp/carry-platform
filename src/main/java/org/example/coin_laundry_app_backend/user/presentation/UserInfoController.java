4a1aee9641eeb6ee8a6335a24ccd8e2063422c4e
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-10 00:02:35 +0900

    사용자 약관 상태 조회 및 각 약관에 대한 동의/철회 API 구현 - `UserInfoController`

--- src/main/java/org/example/coin_laundry_app_backend/user/presentation/UserInfoController.java
+++ src/main/java/org/example/coin_laundry_app_backend/user/presentation/UserInfoController.java
@@ -1,22 +1,45 @@
 package org.example.coin_laundry_app_backend.user.presentation;
 
 import lombok.RequiredArgsConstructor;
-import org.example.coin_laundry_app_backend.user.application.service.UserService;
-import org.example.coin_laundry_app_backend.user.domain.model.entity.domainmodel.User;
+import org.example.coin_laundry_app_backend.common.presentation.payload.ApiCommonResponse;
+import org.example.coin_laundry_app_backend.user.application.service.UserInfoService;
+import org.example.coin_laundry_app_backend.user.presentation.payload.request.UpdateTermRequest;
+import org.example.coin_laundry_app_backend.user.presentation.payload.response.UserTermsResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-@RequiredArgsConstructor
-@RequestMapping("/users")
 @RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserInfoController {
 
-    private final UserService userService;
+    private final UserInfoService userInfoService;
 
-    @GetMapping
-    public Flux<User> getAllUsers() {
-        return userService.getAllUsers();
+    @GetMapping("/terms")
+    public Mono<ApiCommonResponse<UserTermsResponse>> getUserTermsInfo(
+        @AuthenticationPrincipal Long userId) {
+        Mono<UserTermsResponse> userTermsResponseMono = userInfoService.getUserTermsInfo(userId);
+        return userTermsResponseMono.map(ApiCommonResponse::createSuccessResponse);
+    }
+
+    @PostMapping("/terms/commercial")
+    public Mono<ApiCommonResponse<Void>> setCommercialTermYn(@AuthenticationPrincipal Long userId,
+        @RequestBody UpdateTermRequest request) {
+        Mono<Void> voidMono = userInfoService.setCommercialTermYn(userId, request.getAcceptYn());
+        return voidMono.then(
+            Mono.defer(() -> Mono.just(ApiCommonResponse.createSuccessResponse())));
+    }
+
+    @PostMapping("/terms/location")
+    public Mono<ApiCommonResponse<Void>> setLocationTermYn(@AuthenticationPrincipal Long userId,
+        @RequestBody UpdateTermRequest request) {
+        Mono<Void> voidMono = userInfoService.setLocationTermYn(userId, request.getAcceptYn());
+        return voidMono.then(
+            Mono.defer(() -> Mono.just(ApiCommonResponse.createSuccessResponse())));
     }
 }
