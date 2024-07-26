a491a284d842928dcfe094cdc4637abec6906770
KyungMin Lee <hello.tidavid@gmail.com>
2024-07-26 13:48:51 +0900

    feat: Term 도메인 도메인 모델 구현
    - Term 도메인 도메인 모델 `Term`을 구현했습니다.
      - 약관 이름, 버전을 포함하는 `TermInfo` VO를 사용하도록 구현했습니다.

---
+++ src/main/java/org/example/coin_laundry_app_backend/user/domain/model/entity/domainmodel/Term.java
@@ -0,0 +1,35 @@
+package org.example.coin_laundry_app_backend.user.domain.model.entity.domainmodel;
+
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.example.coin_laundry_app_backend.user.domain.model.entity.data.TermData;
+import org.example.coin_laundry_app_backend.user.domain.model.enums.TermType;
+import org.example.coin_laundry_app_backend.user.domain.model.value.TermInfo;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Term {
+
+    Long id;
+    TermType termType;
+    TermInfo termInfo;
+    String context;
+    LocalDateTime createdAt;
+
+    public static Term from(TermData termData) {
+        TermInfo info = TermInfo.of(termData.getTermInfoTitle(), termData.getTermInfoVersion());
+        return new Term(termData.getId(), termData.getTermType(), info, termData.getContext(),
+            termData.getCreatedAt());
+    }
+
+    public static Term of(TermType termType, TermInfo termInfo, String context) {
+        return new Term(null, termType, termInfo, context, null);
+    }
+
+    public TermData toData() {
+        return new TermData(id, termType, termInfo.getTitle(), termInfo.getVersion(), context,
+            createdAt);
+    }
+}
