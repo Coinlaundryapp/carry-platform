b47ee297b94d8aba50183118ff3b7c54a0b2f793
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-10 21:19:33 +0900

    fix: flyway sql update
    - users table 변경에 따른 flyway sql을 업데이트했습니다.

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,18 +1,23 @@
 -- V1__create_user_table.sql
 CREATE TABLE users
 (
-    id            BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username      VARCHAR(100) NOT NULL,
-    phone_number  VARCHAR(14)  NOT NULL,
-    commercial_yn TINYINT      NOT NULL,
-    location_yn   TINYINT      NOT NULL,
-);
-
-CREATE TABLE verification_codes
-(
-    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
-    phone_number VARCHAR(14)  NOT NULL,
-    code         VARCHAR(100) NOT NULL,
-    created_at   TIMESTAMP    NOT NULL,
-    expired_at   TIMESTAMP    NOT NULL,
+    id                  BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name                VARCHAR(100),
+    nickname            VARCHAR(100),
+    phone_number        VARCHAR(20),
+    kakao_id            BIGINT,
+    thumbnail_image_url VARCHAR(255),
+    profile_image_url   VARCHAR(255),
+    connected_at        TIMESTAMP,
+    has_email           TINYINT(1),
+    is_email_valid      TINYINT(1),
+    is_email_verified   TINYINT(1),
+    email               VARCHAR(100),
+    age_range           VARCHAR(50),
+    has_birthday        TINYINT(1),
+    birthday            varchar(30),
+    birthday_type       varchar(30),
+    gender              varchar(30),
+    ci                  varchar(255),
+    ci_authenticated_at TIMESTAMP
 );
