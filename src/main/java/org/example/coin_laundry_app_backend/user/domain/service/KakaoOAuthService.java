57d11fc1173d2fcd87a10e91048dc4c08924eb4c
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-15 21:20:17 +0900

    feat: 카카오 로그인 인증 로직 구현
    - Authentication Token을 활용해 카카오 로그인 인증을 진행하는 로직을 구현했습니다.
      - 테스트를 위해서는 `application.yml`에
        - `oauth.kakao.client-id`
        - `oauth.kakao.client-secret`
        - `oauth.kakao.redirect-uri`
        를 등록해야합니다

--- src/main/java/org/example/coin_laundry_app_backend/user/domain/service/KakaoOAuthService.java
+++ src/main/java/org/example/coin_laundry_app_backend/user/domain/service/KakaoOAuthService.java
@@ -1,9 +1,12 @@
 package org.example.coin_laundry_app_backend.user.domain.service;
 
+import org.example.coin_laundry_app_backend.user.presentation.payload.response.KakaoAuthenticationResponse;
 import org.example.coin_laundry_app_backend.user.presentation.payload.response.KakaoUnlinkResponse;
 import org.example.coin_laundry_app_backend.user.presentation.payload.response.KakaoUserResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -11,7 +14,33 @@ import reactor.core.publisher.Mono;
 // 고민 포인트: 추상화?
 public class KakaoOAuthService {
 
-    // TODO: baseURL 주입 방식 변경
+    private final String clientId;
+    private final String clientSecret;
+    private final String redirectUri;
+
+    public KakaoOAuthService(@Value("oauth.kakao.client-id") String clientId,
+        @Value("oauth.kakao.client-secret") String clientSecret,
+        @Value("oauth.kakao.redirect-uri") String redirectUri) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.redirectUri = redirectUri;
+    }
+
+    public Mono<KakaoAuthenticationResponse> getKakaoAccessToken(String code) {
+        WebClient webClient = WebClient.builder().baseUrl("https://kauth.kakao.com/oauth/token")
+            .defaultHeaders(
+                headers -> headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED)).build();
+        return webClient.post()
+            .body(BodyInserters
+                .fromFormData("grant_type", "authorization_code")
+                .with("client_id", clientId)
+                .with("redirect_uri", redirectUri)
+                .with("code", code)
+                .with("client_secret", clientSecret))
+            .retrieve()
+            .bodyToMono(KakaoAuthenticationResponse.class);
+    }
+
     public Mono<KakaoUserResponse> getKakaoUserInfo(String accessToken) {
         return getWebClient("https://kapi.kakao.com/v2/user/me", accessToken).get()
             .retrieve()
