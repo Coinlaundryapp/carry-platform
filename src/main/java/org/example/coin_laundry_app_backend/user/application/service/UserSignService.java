fca914a5fdb692c8ce8fa212da35912a77875090
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-28 00:36:31 +0900

    refactor: manageUser 메소드 수정
    - 메서드 매개변수로 redirectUri를 전달받도록 수정했습니다.

--- src/main/java/org/example/coin_laundry_app_backend/user/application/service/UserSignService.java
+++ src/main/java/org/example/coin_laundry_app_backend/user/application/service/UserSignService.java
@@ -39,7 +39,7 @@ public class UserSignService {
     Case2: Carry 서비스에 등록된 사용자가 있다! (로그인 Case)
     -> User 조회 -> JWTToken 발급 -> RefreshToken 발급 -> LoginResponse 반환
     */
-    public Mono<LoginResponse> manageUser(String authenticationCode) {
+    public Mono<LoginResponse> manageUser(String authenticationCode, String redirectUri) {
         return kakaoOAuthService.getKakaoAccessToken(authenticationCode)
             .flatMap(this::processKakaoAuthentication);
     }
