a0f8b6b372a56fc5960842a0a33a3f8300fc7db5
KyungMin Lee <hello.tidavid@gmail.com>
2024-09-15 14:26:22 +0900

    style: NaverGeocodingService
    - modify style of NaverGeocodingService

--- src/main/java/org/example/coin_laundry_app_backend/geo/external/NaverGeocodingService.java
+++ src/main/java/org/example/coin_laundry_app_backend/geo/external/NaverGeocodingService.java
@@ -1,25 +1,20 @@
 package org.example.coin_laundry_app_backend.geo.external;
 
 import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Setter;
-import org.example.coin_laundry_app_backend.geo.domain.model.EPSG4326Coordinate;
 import org.example.coin_laundry_app_backend.geo.application.service.GeocodingService;
 import org.example.coin_laundry_app_backend.geo.application.service.model.GeoModel;
 import org.example.coin_laundry_app_backend.geo.application.service.model.JibunGeoModel;
 import org.example.coin_laundry_app_backend.geo.application.service.model.RoadGeoCoding;
+import org.example.coin_laundry_app_backend.geo.domain.model.EPSG4326Coordinate;
 import org.example.coin_laundry_app_backend.geo.external.recrod.GetGeocodingResponse;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Setter
 @Service
@@ -33,53 +28,44 @@ public class NaverGeocodingService implements GeocodingService {
 
     @PostConstruct
     public void init() {
-        this.webClient = WebClient.builder()
-                .baseUrl(baseUrl)
-                .defaultHeaders((headers) -> {
-                            headers.add("X-NCP-APIGW-API-KEY-ID", apiKeyId);
-                            headers.add("X-NCP-APIGW-API-KEY", apiKey);
-                        }
-                )
-                .build();
+        this.webClient = WebClient.builder().baseUrl(baseUrl).defaultHeaders(headers -> {
+            headers.add("X-NCP-APIGW-API-KEY-ID", apiKeyId);
+            headers.add("X-NCP-APIGW-API-KEY", apiKey);
+        }).build();
     }
 
     @Override
     public Mono<List<GeoModel>> getGeocoding(String address) {
-        return webClient.get()
-            .uri(uriBuilder ->
-                uriBuilder
-                .queryParam("query", address)
-                .build()
-            ).retrieve()
-            .bodyToMono(GetGeocodingResponse.class)
-            .map(response ->
-                response.addresses().stream().map(responseAddress -> new GeoModel(
-                    responseAddress.jibunAddress(),
-                    responseAddress.roadAddress(),
-                    new EPSG4326Coordinate(
-                        Double.parseDouble(responseAddress.y()),
-                        Double.parseDouble(responseAddress.x())
-                    ),
-                    responseAddress.getAddressElementNameOrNull(GetGeocodingResponse.AddressType.SIDO),
-                    responseAddress.getAddressElementNameOrNull(GetGeocodingResponse.AddressType.SIGUGUN),
-                    responseAddress.getAddressElementNameOrNull(GetGeocodingResponse.AddressType.DONGMYUN),
-                    responseAddress.getAddressElementNameOrNull(GetGeocodingResponse.AddressType.RI),
-                    responseAddress.getAddressElementNameOrNull(GetGeocodingResponse.AddressType.ROAD_NAME),
-                    responseAddress.getAddressElementNameOrNull(GetGeocodingResponse.AddressType.BUILDING_NAME),
-                    responseAddress.getAddressElementNameOrNull(GetGeocodingResponse.AddressType.LAND_NUMBER),
-                    responseAddress.getAddressElementNameOrNull(GetGeocodingResponse.AddressType.POSTAL_CODE)
-                    )
-                ).collect(Collectors.toCollection(ArrayList::new))
-            );
+        return webClient.get().uri(uriBuilder -> uriBuilder.queryParam("query", address).build())
+            .retrieve().bodyToMono(GetGeocodingResponse.class).map(
+                response -> response.addresses().stream().map(
+                    responseAddress -> new GeoModel(responseAddress.jibunAddress(),
+                        responseAddress.roadAddress(),
+                        new EPSG4326Coordinate(Double.parseDouble(responseAddress.y()),
+                            Double.parseDouble(responseAddress.x())),
+                        responseAddress.getAddressElementNameOrNull(
+                            GetGeocodingResponse.AddressType.SIDO),
+                        responseAddress.getAddressElementNameOrNull(
+                            GetGeocodingResponse.AddressType.SIGUGUN),
+                        responseAddress.getAddressElementNameOrNull(
+                            GetGeocodingResponse.AddressType.DONGMYUN),
+                        responseAddress.getAddressElementNameOrNull(
+                            GetGeocodingResponse.AddressType.RI),
+                        responseAddress.getAddressElementNameOrNull(
+                            GetGeocodingResponse.AddressType.ROAD_NAME),
+                        responseAddress.getAddressElementNameOrNull(
+                            GetGeocodingResponse.AddressType.BUILDING_NAME),
+                        responseAddress.getAddressElementNameOrNull(
+                            GetGeocodingResponse.AddressType.LAND_NUMBER),
+                        responseAddress.getAddressElementNameOrNull(
+                            GetGeocodingResponse.AddressType.POSTAL_CODE))).toList());
     }
 
     @Override
     public Mono<List<JibunGeoModel>> getJibunGeocoding(String address) {
-        return getGeocoding(address)
-            .map(geoModels -> geoModels.stream()
-                .filter(geoModel -> !geoModel.jibunAddress().isEmpty())
-                .map(geoModel -> new JibunGeoModel(
-                    geoModel.jibunAddress(),
+        return getGeocoding(address).map(
+            geoModels -> geoModels.stream().filter(geoModel -> !geoModel.jibunAddress().isEmpty())
+                .map(geoModel -> new JibunGeoModel(geoModel.jibunAddress(),
                     geoModel.coordinate(),
                     geoModel.sido(),
                     geoModel.sigungu(),
@@ -87,8 +73,8 @@ public class NaverGeocodingService implements GeocodingService {
                     geoModel.ri(),
                     geoModel.buildingName(),
                     geoModel.landNumber(),
-                    geoModel.postalCode()
-                )).collect(Collectors.toCollection(ArrayList::new)));
+                    geoModel.postalCode()))
+                .toList());
     }
 
     @Override
