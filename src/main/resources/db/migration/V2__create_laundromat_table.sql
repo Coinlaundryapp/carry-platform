743c91209b31d99be63869896a41b8dcd10c1ef7
KyungMin Lee <hello.tidavid@gmail.com>
2024-09-21 16:18:04 +0900

    refactor: `~images` to `~media_resources`
    - add new column name `extension`

--- src/main/resources/db/migration/V2__create_laundromat_table.sql
+++ src/main/resources/db/migration/V2__create_laundromat_table.sql
@@ -30,12 +30,13 @@ CREATE TABLE laundromat_media_resources
     id            BIGSERIAL PRIMARY KEY,
     laundromat_id BIGINT REFERENCES laundromats (id) ON DELETE CASCADE,
     media_url     VARCHAR(255)                           NOT NULL,
+    extension     VARCHAR(10)                            NOT NULL,
     created_at    TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
     updated_at    TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
 
-CREATE INDEX idx_laundromat_images_laundromat_id ON laundromat_media_resources (laundromat_id);
-CREATE INDEX idx_laundromat_images_laundromat_id_media_url ON laundromat_media_resources (laundromat_id, media_url);
+CREATE INDEX idx_laundromat_media_resources_laundromat_id ON laundromat_media_resources (laundromat_id);
+CREATE INDEX idx_laundromat_media_resources_laundromat_id_media_values ON laundromat_media_resources (laundromat_id, media_url, extension);
 
 ALTER TABLE shipping_addresses
     ADD COLUMN latitude DOUBLE PRECISION;
@@ -58,11 +59,12 @@ CREATE INDEX idx_reviews_user_id ON reviews (user_id);
 CREATE INDEX idx_reviews_laundromat_id_user_id ON reviews (laundromat_id, user_id);
 CREATE INDEX idx_reviews_rating ON reviews (rating);
 
-CREATE TABLE review_images
+CREATE TABLE review_media_resources
 (
     id        BIGSERIAL PRIMARY KEY,
     review_id BIGINT REFERENCES reviews (id) ON DELETE CASCADE,
-    image_url VARCHAR(255) NOT NULL
+    media_url VARCHAR(255) NOT NULL,
+    extension VARCHAR(10)  NOT NULL
 );
 
-CREATE INDEX idx_review_images_review_id ON review_images (review_id);
+CREATE INDEX idx_review_media_resources_review_id ON review_media_resources (review_id);
