49012b78f3acaf9cdaee8b4b3f5485939568d9b6
KyungMin Lee <hello.tidavid@gmail.com>
2024-09-24 18:06:06 +0900

    Refactor/nearby laundromat api response (#19)
    * refactor: Modified new Response Field
    
    - `laundryPrice` -> `laundryDeliveryFare`
    - added new Field
      - `individualLaundryPrice`
      - `groupLaundryFare`
    
    * feat: Implemented calculate laundry delivery fare logic

--- src/main/java/org/example/coin_laundry_app_backend/laundromat/presentation/payload/response/LaundromatCommonResponse.java
+++ src/main/java/org/example/coin_laundry_app_backend/laundromat/presentation/payload/response/LaundromatCommonResponse.java
@@ -24,9 +24,12 @@ public class LaundromatCommonResponse {
     private final double longitude;
     @Schema(description = "사용자와의 거리 (단위: m)", example = "300.12345")
     private final double distance;
-    // TODO: How Could I get laundryPrice from db?
-    @Schema(description = "세탁 가격", example = "5000")
-    private int laundryPrice;
+    @Schema(description = "배송비", example = "5000")
+    private final int laundryDeliveryFare;
+    @Schema(description = "단독 세탁 가격", example = "5000")
+    private int individualLaundryPrice;
+    @Schema(description = "팀 세탁 가격", example = "4000")
+    private int groupLaundryFare;
 
     // From LaundryOptionMapping Entity
     @Schema(description = "세탁소 옵션 목록")
@@ -50,6 +53,7 @@ public class LaundromatCommonResponse {
         this.latitude = latitude;
         this.longitude = longitude;
         this.distance = distance;
+        this.laundryDeliveryFare = calculateDeliveryFare((int) distance);
         this.options = options;
         this.mediaResources = mediaResources;
     }
@@ -57,6 +61,19 @@ public class LaundromatCommonResponse {
     public void setReviewStatistic(double reviewAverageRating, long reviewCount) {
         this.reviewAverageRating = reviewAverageRating;
         this.reviewCount = reviewCount;
+    }
+
+    private int calculateDeliveryFare(int distance) {
+        int fare = 4000;
+        if (distance <= 100) {
+            return fare;
+        } else if (distance < 1000) {
+            fare += ((distance - 100) / 100) * 300;
+        } else {
+            fare += (900 / 100) * 300;
+            fare += ((distance - 1000) / 100) * 200;
+        }
+        return fare;
     }
 
 }
