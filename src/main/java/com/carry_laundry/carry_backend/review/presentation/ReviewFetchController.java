b8712b47245d0a5cc8189720ffef8b9e4eb61629
KyungMin Lee <hello.tidavid@gmail.com>
2024-10-04 15:56:33 +0900

    refactor: Modified review fetch logic with Cursor Pagination
    - Implemented cursor pagination logic in query
    - separated logic when cursor is valid and invalid

--- src/main/java/com/carry_laundry/carry_backend/review/presentation/ReviewFetchController.java
+++ src/main/java/com/carry_laundry/carry_backend/review/presentation/ReviewFetchController.java
@@ -1,12 +1,13 @@
 package com.carry_laundry.carry_backend.review.presentation;
 
 import com.carry_laundry.carry_backend.common.presentation.payload.ApiCommonResponse;
+import com.carry_laundry.carry_backend.common.presentation.payload.CursorPaginationResponse;
 import com.carry_laundry.carry_backend.review.application.ReviewService;
 import com.carry_laundry.carry_backend.review.presentation.api.ReviewFetchSwagger;
 import com.carry_laundry.carry_backend.review.presentation.payload.response.ReviewCommonResponse;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +20,12 @@ public class ReviewFetchController implements ReviewFetchSwagger {
 
     private final ReviewService reviewService;
 
-    @GetMapping
-    public Mono<ApiCommonResponse<List<ReviewCommonResponse>>> getReviewsByLaundryId(
-        @RequestParam Long laundryId) {
-        return reviewService.getReviewsByLaundryId(laundryId)
-            .collectList()
+    @GetMapping("/laundromat/{laundromatId}")
+    public Mono<ApiCommonResponse<CursorPaginationResponse<ReviewCommonResponse>>> getReviewsByLaundryId(
+        @PathVariable Long laundromatId,
+        @RequestParam(required = false, defaultValue = "0") Long cursor,
+        @RequestParam(required = false, defaultValue = "10") Integer size) {
+        return reviewService.getReviewsByLaundryId(laundromatId, cursor, size)
             .map(ApiCommonResponse::createSuccessResponse);
     }
 }
