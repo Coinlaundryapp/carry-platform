a1cc1fa378feeebd56f2f82bd7a6c6e01d78727d
KyungMin Lee <hello.tidavid@gmail.com>
2024-10-12 16:08:54 +0900

    Refactor: 리뷰 조회 Cursor Pagination 구현 (#26)
    * feat: Implemented CursorPaginationResponse
    
    * refactor: Modified review fetch logic with Cursor Pagination
    
    - Implemented cursor pagination logic in query
    - separated logic when cursor is valid and invalid

--- src/main/java/com/carry_laundry/carry_backend/review/application/ReviewService.java
+++ src/main/java/com/carry_laundry/carry_backend/review/application/ReviewService.java
@@ -1,12 +1,12 @@
 package com.carry_laundry.carry_backend.review.application;
 
+import com.carry_laundry.carry_backend.common.presentation.payload.CursorPaginationResponse;
 import com.carry_laundry.carry_backend.review.presentation.payload.response.ReviewCommonResponse;
 import com.carry_laundry.carry_backend.review.presentation.payload.response.ReviewStatisticResponse;
 import com.carry_laundry.carry_backend.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -21,7 +21,15 @@ public class ReviewService {
     }
 
     @Transactional(readOnly = true)
-    public Flux<ReviewCommonResponse> getReviewsByLaundryId(Long laundryId) {
-        return reviewRepository.getReviewsByLaundromatId(laundryId);
+    public Mono<CursorPaginationResponse<ReviewCommonResponse>> getReviewsByLaundryId(
+        Long laundromatId, Long cursor, Integer size) {
+        if (size < 1) {
+            throw new IllegalArgumentException("Size must be greater than 0");
+        }
+        return reviewRepository.getReviewsByLaundromatId(laundromatId, cursor, size)
+            .collectList()
+            .map(reviewCommonResponses -> CursorPaginationResponse.fromData(reviewCommonResponses,
+                size));
     }
+
 }
--- src/main/java/com/carry_laundry/carry_backend/review/repository/ReviewRepository.java
+++ src/main/java/com/carry_laundry/carry_backend/review/repository/ReviewRepository.java
@@ -10,5 +10,7 @@ import reactor.core.publisher.Mono;
 public interface ReviewRepository {
 
     Mono<ReviewStatisticResponse> getReviewStaticByLaundromatId(Long laundromatId);
-    Flux<ReviewCommonResponse> getReviewsByLaundromatId(Long laundromatId);
+
+    Flux<ReviewCommonResponse> getReviewsByLaundromatId(Long laundromatId, Long reviewId,
+        Integer size);
 }
