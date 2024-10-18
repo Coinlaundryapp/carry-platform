71d9b6b31a714866bc273531b6c2e93513ace245
KyungMin Lee <hello.tidavid@gmail.com>
2024-10-18 10:54:29 +0900

    chore: Modified SecurityConfig
    - Added new pathMatcher for review

--- src/main/java/com/carry_laundry/carry_backend/config/security/SecurityConfig.java
+++ src/main/java/com/carry_laundry/carry_backend/config/security/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig {
                     "/api/v1/addresses/**",
                     "/api/v1/service-availability/**",
                     "/api/v1/prices/**",
-                    "/api/v1/media/*").permitAll()
+                    "/api/v1/media/*",
+                    "/api/v1/reviews/laundromat/*").permitAll()
                 .anyExchange().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
